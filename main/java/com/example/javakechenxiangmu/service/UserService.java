468b8ca0b617a1d3bab9e665bdb10da6472484ab
周武志 <114023848+zhouwuz@users.noreply.github.com>
2022-10-13 10:19:26 +0800

    Delete main directory

--- main/java/com/example/javakechenxiangmu/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.javakechenxiangmu.service;
-
-import com.example.javakechenxiangmu.entity.User;
-import com.example.javakechenxiangmu.support.AccessResult;
-
-public interface UserService {
-    AccessResult loginWithCode(String code);
-//    User getUserByOpenId(String openId);
-}
